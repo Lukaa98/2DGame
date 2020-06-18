c4a9a06aec12d895c3706810aedf404a1b7d21c5
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-06-18 04:56:26 -0400

    obstacles, movement, borders
    added Box obstacles, bird's flying movement  by clicking bird and thorowing.
    added borders and if bird hits the border at x, -x ,y, -y axes game restarts.

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -1,18 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Bird : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Vector3 _initialPosition;
+    private bool _birdWasLaunched;
+    private float _timeSittingAround;
+   
+    [SerializeField] private float _lauchPower = 500;
+
+
+    //Awake is used to initialize any variables or game state before the game starts.
+    //Awake is called only once during the lifetime of the script instance. 
+    private void Awake()
     {
-        
+        _initialPosition = transform.position;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(_birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timeSittingAround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 16.3|| 
+            transform.position.y < -15||
+            transform.position.x > 22 ||
+            transform.position.x < -22 ||
+            _timeSittingAround > 3)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private void OnMouseDown()  //built in method called when mouse is clicked (this case bird gets red)
+    {
+        GetComponent<SpriteRenderer>().color = Color.red; //GetComponent is built in method to 
+                                                          //acces components inside unity
+    }
+
+    private void OnMouseUp() // //built in method called when mouse is unclicked (this case bird gets white)
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 dirictionToInitialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(dirictionToInitialPosition * _lauchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdWasLaunched = true;
+
+    }
+
+    private void OnMouseDrag() //OnMouseDrag is called when the user has clicked on a Collider and is still holding down the mouse.
+                                //OnMouseDrag is called every frame while the mouse is down.
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
     }
 }
