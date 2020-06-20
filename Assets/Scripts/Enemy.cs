3977db8f303884d7f84443bdecef7090963434b0
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-06-20 03:08:56 -0400

    added enemy scrpit
    with enemy script we can destory monsters by collision between birds and monsters

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision) //Sent when an incoming collider makes contact 
+                                                           //with this object's collider (2D physics only).
+                                                           //the Code between braces will be executed when 
+                                                           //two objects collides with each other
+       
+        Bird bird = collision.collider.GetComponent<Bird>();
+        if (bird != null) //null means it does not exists
+        {
+            Destory(gameObjcet);
+        }
+
+}
