b5101412575c2028b57b4f7308a1098e310aa6f7
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-06-20 03:41:38 -0400

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,11 +8,24 @@ public class Enemy : MonoBehaviour
                                                            //with this object's collider (2D physics only).
                                                            //the Code between braces will be executed when 
                                                            //two objects collides with each other
-       
+      { 
         Bird bird = collision.collider.GetComponent<Bird>();
         if (bird != null) //null means it does not exists
         {
-            Destory(gameObjcet);
+            Destroy(gameObject);
+            return;//if we hit object and in this case (mosnter) is destroyed, a return fucntion will end game
+                    //insead of going to the next method which checks if box killed monster or not. 
         }
+
+        Enemy enemy = collision.collider.GetComponent<Enemy>();//if collision was not from the bird and maybe 
+                                                               // it was from the box and it hit from the top
+                                                               //then end game as well
+        if (enemy != null)
+        {
+            return;
+        }
+
+        
+      }
 
 }
