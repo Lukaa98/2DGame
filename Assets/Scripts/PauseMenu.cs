f17627f01df76dc336ca93cae188f4b083434150
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-06-21 03:18:35 -0400

    Update PauseMenu.cs

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -9,15 +9,19 @@ public class PauseMenu : MonoBehaviour
     public GameObject PauseButton;
 
     public void Pause() 
+
     {
+        Pausemenu.SetActive (true);  
         Time.timeScale = 0;
     }
 
     public void Resume() 
     {
+        Pausemenu.SetActive (false);
+        PauseButton.SetActive (true);
         Time.timeScale = 1;
     }
-
+ 
 
 
 }
