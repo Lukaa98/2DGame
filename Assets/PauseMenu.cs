1cf45d85cbf52f348411864083246be1d7d1266a
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-06-21 02:52:19 -0400

    added main menu picture

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject Pausemenu;
+    public GameObject PauseButton;
+
+    public void Pause() 
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Resume() 
+    {
+        Time.timeScale = 1;
+    }
+
+
+
+}
