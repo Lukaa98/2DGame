fece769aa35136fd46363217c117674bc83503c0
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-06-29 02:44:03 -0400

    Update Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,18 +7,20 @@ public class Ball : MonoBehaviour
     private bool isPressed;
 
     private float releaseDelay;
-    private float maxDragDistance = 2f;
+    private float maxDragDistance = 10f;
 
 
     private Rigidbody2D rb;
     private SpringJoint2D sj;
     private Rigidbody2D slingRb;
+    private LineRenderer lr;
 
     private void Awake() 
     {
         rb = GetComponent<Rigidbody2D>();
         sj = GetComponent<SpringJoint2D>();
         slingRb = sj.connectedBody;
+        lr = GetComponent<LineRenderer>();
 
         releaseDelay = 1/(sj.frequency * 4);
 
@@ -37,10 +39,27 @@ public class Ball : MonoBehaviour
 
     private void DragBall() 
     {
+        SetLineRendererPosition();
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        rb.position = mousePosition;
-        fload distance = Vector2.Distance(mousePosition, slingRb.position);
+        float distance = Vector2.Distance(mousePosition, slingRb.position);
 
+        if (distance > maxDragDistance)
+        {
+            Vector2 direction = (mousePosition - slingRb.position).normalized;
+            rb.position = slingRb.position + direction * maxDragDistance;
+        }
+            
+        else {
+                rb.position = mousePosition;
+             }
+        }
+
+    private void SetLineRendererPosition()
+    {
+        Vector3[] positions = new Vector3[2];
+        positions[0] = rb.position;
+        position[1] = hookRb.position;
+        lr.SetPosition(positions);
     }
 
     private void OnMouseDown()
