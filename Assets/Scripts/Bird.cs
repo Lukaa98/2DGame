c50f9b34316fdfa72ac305a8bd72b123aa44b9ed
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-06-29 05:57:03 -0400

    Update Bird.cs

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,7 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-
-
 public class Bird : MonoBehaviour
 {
     private bool isClicked = false;
@@ -10,43 +8,32 @@ public class Bird : MonoBehaviour
     private float _timeSittingAround;
    
     [SerializeField] private float _lauchPower = 500;
-
-
     //Awake is used to initialize any variables or game state before the game starts.
     //Awake is called only once during the lifetime of the script instance. 
     private void Awake()
     {
         _initialPosition = transform.position;
-
     }
-
     private void Update()
     {
-
         GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
                 GetComponent<LineRenderer>().SetPosition(0, transform.position);
-
-
         if(_birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
         {
             _timeSittingAround += Time.deltaTime;
         }
-
         if (transform.position.y > 30|| 
             transform.position.y < -50 ||
             transform.position.x > 65 ||
             transform.position.x < -70 ||
-            _timeSittingAround > 300)
+            _timeSittingAround > 2)
+
         {
             string currentSceneName = SceneManager.GetActiveScene().name;
             SceneManager.LoadScene(currentSceneName);
         }
        
     }
-
-
-
-
     private void OnMouseDown()  //built in method called when mouse is clicked (this case bird gets red)
     {
         if(!isClicked)
@@ -56,23 +43,19 @@ public class Bird : MonoBehaviour
         GetComponent<LineRenderer>().enabled = true;
         }
     }
-
     private void OnMouseUp() // //built in method called when mouse is unclicked (this case bird gets white)
     {
         if(!isClicked)
         {
         GetComponent<SpriteRenderer>().color = Color.white;
-
         Vector2 dirictionToInitialPosition = _initialPosition - transform.position;
         GetComponent<Rigidbody2D>().AddForce(dirictionToInitialPosition * _lauchPower);
         GetComponent<Rigidbody2D>().gravityScale = 1;
         _birdWasLaunched = true;
-
         GetComponent<LineRenderer>().enabled = false;
         isClicked=true;
         }
     }
-
     private void OnMouseDrag() //OnMouseDrag is called when the user has clicked on a Collider and is still holding down the mouse.
                                 //OnMouseDrag is called every frame while the mouse is down.
     {
