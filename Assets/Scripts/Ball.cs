41e70486c349395ce5ed863e134acfd02828bb57
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-06-29 01:37:53 -0400

    Create Ball.cs

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private bool isPressed;
+    private Rigidbody2D rb;
+
+    private void Awake() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed) {
+            DragBall();
+        }
+        
+    }
+
+    private void DragBall() 
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rb.position = mousePosition;
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+                rb.isKinematic = true;
+
+
+    }
+}
