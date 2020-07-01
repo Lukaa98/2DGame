be2184209990cbb67ced86931d534642584436e8
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-06-30 21:37:35 -0400

    Create Inventory.cs
    counts how many enemys were killed

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory Reference;
+    public int KilledEnemys;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Reference = this;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
