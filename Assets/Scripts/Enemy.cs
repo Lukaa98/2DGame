173af6d775726f5d5f3e4835db740aa328cce06f
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-06-30 21:53:10 -0400

    Update Enemy.cs
    level goes to next scene after enemys are dead

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
+
+    public int EnemysToKill = 2;
     private void OnCollisionEnter2D(Collision2D collision) //Sent when an incoming collider makes contact 
                                                            //with this object's collider (2D physics only).
                                                            //the Code between braces will be executed when 
@@ -13,9 +15,13 @@ public class Enemy : MonoBehaviour
         Bird bird = collision.collider.GetComponent<Bird>();
         if (bird != null) //null means it does not exists
         {
+            Inventory.Reference.KilledEnemys += 1;
             Destroy(gameObject);
-            return;//if we hit object and in this case (mosnter) is destroyed, a return fucntion will end game
+           //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+          //  return;//if we hit object and in this case (mosnter) is destroyed, a return fucntion will end game
                     //insead of going to the next method which checks if box killed monster or not. 
+
         }
 
         Enemy enemy = collision.collider.GetComponent<Enemy>();//if collision was not from the bird and maybe 
@@ -27,14 +33,19 @@ public class Enemy : MonoBehaviour
         }
            if (collision.contacts[0].normal.y < -0.5)  //if we hit from the top
            {
+             Inventory.Reference.KilledEnemys += 1;
+
                Destroy(gameObject);
+
            }
 
-        if(enemy == null)
-        {
-              SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+           if(Inventory.Reference.KilledEnemys >= EnemysToKill)
+           {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
-        }
+           }
+
+        
       }
 
 }
