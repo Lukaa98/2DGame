29673767e26deac0212d1cec328beaff3fae8579
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-07-13 23:54:45 -0400

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,54 +5,26 @@ using UnityEngine.SceneManagement;
 public class Enemy : MonoBehaviour
 {
   public Ball ballobj = null;
-      public GameObject levelLost;
-
-
-    public float EnemysToKill = 2f;
-    private float killedago;
-
-    //public GameObject levelwon;
-    private void OnCollisionEnter2D(Collision2D collision) //Sent when an incoming collider makes contact 
+  public GameObject levelLost;
+  public float EnemysToKill = 2f;
+  private float killedago;
+  private void OnCollisionEnter2D(Collision2D collision) //Sent when an incoming collider makes contact 
                                                            //with this object's collider (2D physics only).
                                                            //the Code between braces will be executed when 
                                                            //two objects collides with each other
       { 
-        Bird bird = collision.collider.GetComponent<Bird>();
-        if (bird != null) //null means it does not exists
-        {
-            Destroy(gameObject);
-            Inventory.Reference.KilledEnemys += 1;
-           //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-          //  return;//if we hit object and in this case (mosnter) is destroyed, a return fucntion will end game
-                    //insead of going to the next method which checks if box killed monster or not. 
+       
 
-        }
-
-         Ball ball = collision.collider.GetComponent<Ball>();
+        Ball ball = collision.collider.GetComponent<Ball>();
         if (ball != null) //null means it does not exists
         {
             Destroy(gameObject);
             Inventory.Reference.KilledEnemys += 1;
-
-           //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
-          //  return;//if we hit object and in this case (mosnter) is destroyed, a return fucntion will end game
-                    //insead of going to the next method which checks if box killed monster or not. 
-
         }
 
         Enemy enemy = collision.collider.GetComponent<Enemy>();//if collision was not from the bird and maybe 
                                                                // it was from the box and it hit from the top
                                                                //then end game as well
-        if (enemy != null)
-        {
-            return;
-        }
-           if (collision.contacts[0].normal.y < -0.5)  //if we hit from the top
-           {
-               Destroy(gameObject);
-               Inventory.Reference.KilledEnemys += 1;
-           }
            
              if(Inventory.Reference.KilledEnemys >= EnemysToKill && ballobj != null)
            { 
