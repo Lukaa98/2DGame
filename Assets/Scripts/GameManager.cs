8327378d2ddb036dde75bad4eef12df4529f6350
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-07-14 18:49:01 -0400

    Create GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject levelwon;
+    public GameObject levelLost;
+    public GameObject RestartButton;
+    public GameObject PauseButton;
+    
+    bool controlBool = false;
+
+    
+
+public void LevelWon()
+
+ {
+
+    PauseButton.SetActive (false); 
+    RestartButton.SetActive (false); 
+    controlBool=true;
+    levelwon.SetActive(true);
+    Debug.Log("game Won");
+
+ }
+
+public void LevelLost()
+
+ {
+   if(controlBool == false){
+ 
+    PauseButton.SetActive (false); 
+    RestartButton.SetActive (false); 
+    levelLost.SetActive(true);
+    Debug.Log("level los12t ");
+                           }
+ }
+}
