37eed4de491491a8a1a38438e2dd27d443524117
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-07-14 00:28:55 -0400

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,8 @@ public class Enemy : MonoBehaviour
   public GameObject levelLost;
   public float EnemysToKill = 2f;
   private float killedago;
+
+
   private void OnCollisionEnter2D(Collision2D collision) //Sent when an incoming collider makes contact 
                                                            //with this object's collider (2D physics only).
                                                            //the Code between braces will be executed when 
@@ -16,15 +18,27 @@ public class Enemy : MonoBehaviour
        
 
         Ball ball = collision.collider.GetComponent<Ball>();
-        if (ball != null) //null means it does not exists
+               Enemy enemy = collision.collider.GetComponent<Enemy>();//if collision was not from the bird and maybe 
+
+        if (ball != null || collision.contacts[0].normal.y < -0.5) //null means it does not exists
         {
             Destroy(gameObject);
             Inventory.Reference.KilledEnemys += 1;
         }
 
-        Enemy enemy = collision.collider.GetComponent<Enemy>();//if collision was not from the bird and maybe 
+      // Enemy enemy = collision.collider.GetComponent<Enemy>();//if collision was not from the bird and maybe 
                                                                // it was from the box and it hit from the top
-                                                               //then end game as well
+
+      //if  (enemy != null)
+      //{
+       // return;
+      //}        
+        
+         //if (collision.contacts[0].normal.y < -0.5)  //if we hit from the top
+          //{
+           //   Destroy(gameObject);
+             //  Inventory.Reference.KilledEnemys += 1;
+           //}                                                    //then end game as well
            
              if(Inventory.Reference.KilledEnemys >= EnemysToKill && ballobj != null)
            { 
