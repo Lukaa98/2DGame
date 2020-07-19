cc20961bbe30a89d6818f6417c54c20ce1590a47
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-07-19 00:03:49 -0400

    Update AudioManager.cs

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -6,8 +6,19 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
     // Start is called before the first frame update
+
+    public static AudioManager instance;
     void Awake()
     {
+        if(instance == null)
+            instance = this;
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+        DontDestroyOnLoad(gameObject);
+
         foreach (Sound s in sounds)
         {
            s.source = gameObject.AddComponent<AudioSource>();
