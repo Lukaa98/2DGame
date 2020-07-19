4f3b9a3850d6f5a973087524a383431a031d2249
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-07-18 23:58:47 -0400

    Update AudioManager.cs

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -15,15 +15,28 @@ public class AudioManager : MonoBehaviour
 
            s.source.volume = s.volume;
            s.source.pitch = s.pitch;
+           s.source.loop = s.loop;
 
 
         }
-        
     }
+
+        void Start ()
+        {
+            Play("Theme");
+        }
+        
+    
 
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null )
+        {
+            Debug.LogWarning("sound: " + name + " not found ");
+        return;
+        }
+
         s.source.Play();
     }
 }
