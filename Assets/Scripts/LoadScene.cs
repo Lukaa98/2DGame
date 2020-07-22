6dcf6c037ae666dbc297cd4dad00402997e1faa0
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-07-21 23:45:08 -0400

    Update LoadScene.cs

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -10,7 +10,10 @@ public class LoadScene : MonoBehaviour
     {
                Time.timeScale = 1;
 
+
         SceneManager.LoadScene(SceneIndex);
+                    FindObjectOfType<AudioManager>().Play("Clicking Button");
+
 
     }
 
