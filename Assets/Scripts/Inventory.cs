bcbc2d4ea0852a60e3af6f0e55a9f05201c1221b
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-08-07 22:04:28 -0400

    Update Inventory.cs

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    
     public static Inventory Reference;
     public int KilledEnemys;
-    public static Inventory Reference1;
-    public int ThrownBirds;
     // Start is called before the first frame update
     void Awake()
     {
         Reference = this;
-        Reference1 = this;
         
     }
 
