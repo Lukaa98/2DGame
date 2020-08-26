14b44e8bf10612089c8b2f06733e785df187fc20
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-08-25 21:53:51 -0400

    Create Bomb.cs

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class Bomb : MonoBehaviour
+{
+    
+
+    public float fieldOfImpact;
+    public float force;
+
+    public LayerMask LayerToHit;
+    // Start is called before the first frame update
+   
+    
+    
+  private void OnCollisionEnter2D(Collision2D collision) //Sent when an incoming collider makes contact 
+{
+
+        Bomb bomb = collision.collider.GetComponent<Bomb>();
+
+        if (collision.contacts[0].normal.y < -0.5 )
+        {
+                Destroy(gameObject);
+                Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position,fieldOfImpact,LayerToHit);
+
+                    foreach(Collider2D obj in objects)
+                        {
+                            Vector2 direction = obj.transform.position - transform.position;
+                            obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
+                        }
+
+        }
+}
+
+
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,fieldOfImpact);
+    }
+
+
+    
+}
