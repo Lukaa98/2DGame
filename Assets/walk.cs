cbfd16c7a0c29fb23e639f555f15b4b0fd7545ac
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-08-28 23:36:17 -0400

    Create walk.cs

---
+++ Assets/walk.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private float dirx, diry, moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = 5f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirx = Input.GetAxisRaw(" hor") * moveSpeed;
+        diry = Input.GetAxisRaw(" ver" ) * moveSpeed;
+ 
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2 (dirx, diry);
+    }
+}
