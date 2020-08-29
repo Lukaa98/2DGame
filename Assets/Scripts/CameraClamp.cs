53648af245ec2a01f4691d128d57617dd6b13cb7
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2020-08-28 23:36:15 -0400

    Create CameraClamp.cs

---
+++ Assets/Scripts/CameraClamp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClamp : MonoBehaviour
+{
+   [SerializeField]
+   private Transform targetToFollow;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(targetToFollow.position.x, -75f, 40f),
+            Mathf.Clamp(targetToFollow.position.y, 83f, -40f),
+            transform.position.z);
+
+    
+    }
+}
